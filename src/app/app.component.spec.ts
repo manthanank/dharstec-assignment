1c63a60aba2a623f22c45438fc9a5ac7b634aa0f
Manthan Ankolekar <manthan.ank@gmail.com>
2023-02-08 20:59:10 +0530

    updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'test-assignment'`, () => {
+  it(`should have as title 'dharstec-assignment'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-assignment');
+    expect(app.title).toEqual('dharstec-assignment');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('test-assignment app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('dharstec-assignment app is running!');
   });
 });
